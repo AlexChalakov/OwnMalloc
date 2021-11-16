02605e6d0b9f9f6b0309a9dfa4550889b9f4ad05
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-11-16 14:29:44 +0000

    init commit

---
+++ malloc.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int * freePointer = NULL;
+void nextMemory;
+
+void * new_malloc(size_t size){
+
+};
+
+void new_free(void * ptr){
+
+    return new_free;
+};
+
+int main(){
+    //new_malloc();
+    //new_free();
+}
