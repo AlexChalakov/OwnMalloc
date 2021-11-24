4102ebb2e72a2185ec01878464b2f6fac25c0094
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-11-24 13:09:24 +0000

    init commit with if statements

--- malloc.c
+++ malloc.c
@@ -1,18 +1,57 @@
 #include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 
-int * freePointer = NULL;
-void nextMemory;
+typedef struct dataBlocks {
+    //data about previous block
+    int prevUsed;
+    size_t prevLength;
 
-void * new_malloc(size_t size){
+    //data about next block
+    int nextUsed;
+    size_t nextLength;
 
+    //defining struct pointers
+    struct dataBlocks* prevBlock;
+    struct dataBlocks* nextBlock;
+} dataBlocks;
+
+static dataBlocks head;
+static int* tail;
+
+//void * new_malloc(size_t size); //declare functions
+//void new_free(void * ptr); 
+
+void * new_malloc(size_t size)
+{
+    if (head.nextUsed == NULL) //if the head of the next block is null
+    {
+        head.prevUsed -= 1; //decrease by 1 as we jump off the previous one
+        head.prevLength -= 1;
+
+        head.nextUsed = 0; //define the new block
+        head.nextLength = 0;
+    }
+    
+    if (tail == NULL) //if tail is null
+    {
+        tail = (int*) sbrk(0);  //request more memory space at the tail
+    } 
+
+    if (size < 8192)
+    {
+        
+    }
+    
 };
 
-void new_free(void * ptr){
+void new_free(void * ptr)
+{
 
-    return new_free;
 };
 
 int main(){
-    //new_malloc();
-    //new_free();
+    void * printOne = new_malloc(0);
+    printf("Address: %p\n", printOne);
 }
