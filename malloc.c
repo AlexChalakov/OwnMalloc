5304aecaa937f93555ab30203576d01a924673e4
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-11-26 20:46:27 +0000

    prints commit, VM testing

--- malloc.c
+++ malloc.c
@@ -27,25 +27,29 @@ void * new_malloc(size_t size)
 {
     if (size <= 0) //making sure that the size cant be less or equal to 0
     {
+        printf("Size CANNOT be 0! ERROR!\n");
         return NULL;
     }
     
     if(tail == NULL){
+        printf("Tail has gone to hell! Program break.\n");
         tail = (int*) sbrk(0);  //assign the tail as the program break - end
     }
 
     if (size < SMALL_BLOCK_MAX_SIZE) //define max size, should be less than 8192
     {
+        printf("Size is smaller than 8192.\n");
        if (addr->nextUsed == 0)  //if not used
        {
-           if (addr->nextLength == 0 || size < addr->nextLength) //if the block fits
+           if (size > addr->nextLength) //check if there is enough memory for block to fit, if not
+           {
+               tail = sbrk(((int*)addr->nextBlock + addr->nextLength) - tail); //assign more memory
+           } else if (addr->nextLength == 0 || size < addr->nextLength) //if the block fits and there is enough memory
            {
                addr->nextUsed = 1; //make it used
                addr->nextLength = size; //assign the size at the lengths head adress
            }
-           
        }
-       
     } // else if (mmap)
 };
 
