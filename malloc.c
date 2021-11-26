145f6c705b63a68214a1ee6ca628a7ba60162df8
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-11-26 19:51:09 +0000

    Assign memory checkpoint

--- malloc.c
+++ malloc.c
@@ -3,10 +3,12 @@
 #include <stddef.h>
 #include <stdio.h>
 
+#define SMALL_BLOCK_MAX_SIZE 8192 //defining the max size for the small blocks - maximum of sbrk call
+
 typedef struct dataBlocks {
-    //data about previous block
-    int prevUsed;
-    size_t prevLength;
+    //data about current block
+    int used;
+    size_t length;
 
     //data about next block
     int nextUsed;
@@ -18,40 +20,42 @@ typedef struct dataBlocks {
 } dataBlocks;
 
 static dataBlocks head;
+static dataBlocks* addr = &head;
 static int* tail;
-
-//void * new_malloc(size_t size); //declare functions
-//void new_free(void * ptr); 
 
 void * new_malloc(size_t size)
 {
-    if (head.nextUsed == NULL) //if the head of the next block is null
+    if (size <= 0) //making sure that the size cant be less or equal to 0
     {
-        head.prevUsed -= 1; //decrease by 1 as we jump off the previous one
-        head.prevLength -= 1;
-
-        head.nextUsed = 0; //define the new block
-        head.nextLength = 0;
+        return NULL;
     }
     
-    if (tail == NULL) //if tail is null
-    {
-        tail = (int*) sbrk(0);  //request more memory space at the tail
-    } 
-
-    if (size < 8192)
-    {
-        
+    if(tail == NULL){
+        tail = (int*) sbrk(0);  //assign the tail as the program break - end
     }
-    
+
+    if (size < SMALL_BLOCK_MAX_SIZE) //define max size, should be less than 8192
+    {
+       if (addr->nextUsed == 0)  //if not used
+       {
+           if (addr->nextLength == 0 || size < addr->nextLength) //if the block fits
+           {
+               addr->nextUsed = 1; //make it used
+               addr->nextLength = size; //assign the size at the lengths head adress
+           }
+           
+       }
+       
+    } // else if (mmap)
 };
 
 void new_free(void * ptr)
 {
-
+    ptr = ptr - 7; //getting the head of our pointer
+    dataBlocks freeBlock = (dataBlocks) ptr; //making the pointer be a free block
+    freeBlock->nextUsed = 0;    //making the free block false, as not used
 };
 
 int main(){
-    void * printOne = new_malloc(0);
-    printf("Address: %p\n", printOne);
+    
 }
